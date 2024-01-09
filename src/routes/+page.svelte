0a21a3e745750f3940a306cc7e542d6108962f7e
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-01-08 20:14:54 -0500

    no form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,51 @@
-<h1>yoho</h1>
+<script>
+	import TranslationList from '$components/TranslationList.svelte';
+
+	const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+
+	let input_text = '';
+
+	let translationHistory = [
+		{ en: 'hiya', es: 'hola', ru: 'привет', it: 'ciao' },
+	];
+	let is_loading = false;
+	$: is_ready = input_text.length > 0 && !is_loading;
+
+	async function handleSubmit() {
+		is_loading = true; // Start loading
+		const text = input_text;
+		const apiUrl = 'https://translate.xces.workers.dev';
+
+		try {
+			const response = await fetch(apiUrl, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					api_key: OPENAI_API_KEY
+				},
+				body: JSON.stringify({ text })
+			});
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			const data = await response.json();
+
+			translationHistory = [...translationHistory, data];
+			// Clear input text
+			input_text = '';
+		} catch (error) {
+			console.error('Error fetching translation:', error);
+		} finally {
+			is_loading = false;
+		} // Done loading
+	}
+</script>
+
+
+
+<TranslationList {translationHistory} />
 
 <style>
 	:root {
