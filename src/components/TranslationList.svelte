abe2e7420f3cf5eaffe41acc7112d376a3deaf71
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-03-03 14:53:13 +0100

    data table for translations

--- src/components/TranslationList.svelte
+++ src/components/TranslationList.svelte
@@ -1,16 +1,63 @@
 <script>
-  export let translationHistory =  [];
+	import { createTable, Render, Subscribe } from "svelte-headless-table";
+  import { readable } from "svelte/store";
+	import * as Table from "$components/ui/table";
+
+  /**
+	 * @type {string[]}
+	 */
+   export let translationHistory =  [];
   
-	export let languages = ['en', 'es', 'ru', 'it'];
+	export let languages = ['en', 'es', 'ru', 'it', 'ca', 'de'];
 	$: if (translationHistory.length > 0) {
 		languages = Object.keys(translationHistory[0]);
 	}
 
 	$: console.log(translationHistory);
+	const table = createTable(readable(translationHistory));
+	const columns = table.createColumns(
+		languages.map(lang => (table.column({ accessor: lang, header: lang })))
+	);
+	const { headerRows, pageRows, tableAttrs, tableBodyAttrs } =
+    table.createViewModel(columns);
 
 </script>
 
-<table>
+<div class="rounded-md border">
+  <Table.Root {...$tableAttrs}>
+    <Table.Header>
+      {#each $headerRows as headerRow}
+        <Subscribe rowAttrs={headerRow.attrs()}>
+          <Table.Row>
+            {#each headerRow.cells as cell (cell.id)}
+              <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
+                <Table.Head {...attrs}>
+                  <Render of={cell.render()} />
+                </Table.Head>
+              </Subscribe>
+            {/each}
+          </Table.Row>
+        </Subscribe>
+      {/each}
+    </Table.Header>
+    <Table.Body {...$tableBodyAttrs}>
+      {#each $pageRows as row (row.id)}
+        <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+          <Table.Row {...rowAttrs}>
+            {#each row.cells as cell (cell.id)}
+              <Subscribe attrs={cell.attrs()} let:attrs>
+                <Table.Cell {...attrs}>
+                  <Render of={cell.render()} />
+                </Table.Cell>
+              </Subscribe>
+            {/each}
+          </Table.Row>
+        </Subscribe>
+      {/each}
+    </Table.Body>
+  </Table.Root>
+</div>
+<!-- <table>
   <thead>
     <tr>
       {#each languages as language (language)}
@@ -27,7 +74,7 @@
       </tr>
     {/each}
   </tbody>
-</table>
+</table> -->
 
 <style>
   table {
