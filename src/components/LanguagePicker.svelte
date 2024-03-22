47ab7bb3fcb7de0e9e3ce7129f42057c2e86300c
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-03-22 15:58:25 +0100

    collapsible picker (basic)

--- src/components/LanguagePicker.svelte
+++ src/components/LanguagePicker.svelte
@@ -1,21 +1,41 @@
 <script>
 	import { Checkbox } from '$components/ui/checkbox';
 	import { Label } from '$components/ui/label';
+	import * as Collapsible from "$components/ui/collapsible";
 
 	export let languages = ['en', 'es', 'ru', 'it', 'ca', 'de'];
+	const cataloniaFlagEmoji = String.fromCodePoint(0x1F3F4, 0xE0065, 0xE0073, 0xE0063, 0xE0074, 0xE007F);
+	console.log(cataloniaFlagEmoji);
+
+	let flags = {
+		en: '🇺🇸',
+		es: '🇪🇸',
+		ru: '🇷🇺',
+		it: '🇮🇹',
+		ca: cataloniaFlagEmoji,
+		de: '🇩🇪'
+	};
+	
 	let language_selections = {};
 	languages.forEach(language => language_selections[language] = false);
 	$: selected_languages = Object.keys(language_selections).filter(language => language_selections[language]);
 </script>
 
 <div class="language-picker">
-
-	{#each languages as language}
-	<div>
+	<Collapsible.Root>
+		<Collapsible.Trigger>
+			{#each languages as language}
+				{flags[language]}
+			{/each}
+		</Collapsible.Trigger>
+		<Collapsible.Content>
+		{#each languages as language}
+		<div>
 			<Checkbox bind:checked={language_selections[language]}>{language}</Checkbox>
-			<Label for={language}>{language}</Label>
+			<Label for={language}>{flags[language]} {language}</Label>
 		</div>
-	{/each}
-</div>
+		{/each}
+	</Collapsible.Content>
+	</Collapsible.Root>
+	</div>
 
-<p>{selected_languages}</p>
