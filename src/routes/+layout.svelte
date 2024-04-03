a8415fa4b3e1fc6aaded1ef6f1008c83292088b3
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-04-03 16:32:02 +0200

    favicon yolo commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>import "../app.pcss";</script>
+<svelte:head>
+<link rel="icon" href="/favicon.ico" sizes="32x32">
+<link rel="icon" href="/icon.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" href="/icon-180.png">
+<link rel="manifest" href="/manifest.webmanifest">
+</svelte:head>
+<slot></slot>
