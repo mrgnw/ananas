1124d2c73921ef0f7ade9c96c5ac256889c82fd7
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-04-27 02:52:45 +0200

    Update ApiKeyInput.svelte

--- src/jibs/ApiKeyInput.svelte
+++ src/jibs/ApiKeyInput.svelte
@@ -1,25 +1,26 @@
 <script>
 	export let value = '';
-  function apiKeyIsValid(form_value) {
-		return form_value.startsWith('sk-') && form_value.length === 51;
-	}
-	$: isValid = apiKeyIsValid(value);
+  $: isValid = value.startsWith('sk-') && value.length === 51;
+	let pattern = "sk-[a-zA-Z0-9]{48,48}";
 
 </script>
 
 <div>
-	<!-- This should work, no? -->
-	<!-- pattern="sk-[a-zA-Z0-9]{48,48}" -->
-  <input type="text" 
-	bind:value 
-	placeholder="Enter your API key" 
-	class:valid-key={isValid} 
-	class:invalid-key={!isValid} 
+	<input type="text" 
+		pattern
+		bind:value 
+		size="50"
+		placeholder="Enter your API key" 
+		class:valid-key={isValid} 
+		class:invalid-key={!isValid} 
 	/>
 </div>
 
 
 <style>
+	input {
+		font-family: monospace;
+	}
   .valid-key {
     border-color: green;
   }
