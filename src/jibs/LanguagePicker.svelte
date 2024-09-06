93b06bd71c6b22b718d593a9a12d7ad59ad37c0d
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-09-06 03:38:53 +0200

    Update LanguagePicker.svelte

--- src/jibs/LanguagePicker.svelte
+++ src/jibs/LanguagePicker.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
 	
 	import Check from "lucide-svelte/icons/check";
-	import ChevronsUpDown from "lucide-svelte/icons/chevrons-up-down";
-	
-	// import * as Command from "$lib/components/ui/command/index.ts";
-	// import * as Popover from "$lib/components/ui/popover/index.ts";
 	import { Combobox } from "bits-ui";
-	import { Input } from "$lib/components/ui/input/index.js";
-	import { Button } from "$lib/components/ui/button/index.ts";
-	import { cn } from "$lib/utils.ts";
-	import { tick } from "svelte";
 	import { languages } from 'countries-list';
 	import LanguageList from './LanguageList.svelte';
 
-	$effect(() => {
-		console.debug("languages", languages);
-	})
 
 	// TODO: confirm that this is reactive
 	let { translate_languages = $bindable([]) } = $props();
