5ce8d17369db562ab62683ad2ff73b28e4ed8302
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-10-18 14:42:47 +0200

    basic register/login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { enhance } from '$app/forms'
+  import { login } from '@passlock/sveltekit'
+
+  import {
+    PUBLIC_PASSLOCK_TENANCY_ID,
+    PUBLIC_PASSLOCK_CLIENT_ID
+  } from '$env/static/public'
+
+  const { onSubmit } = login({ 
+    tenancyId: PUBLIC_PASSLOCK_TENANCY_ID, 
+    clientId: PUBLIC_PASSLOCK_CLIENT_ID
+  })
+</script>
+
+<form method="post" use:enhance={onSubmit}>
+  Email:
+  <input type="text" name="email" />
+  <br />
+
+  <button type="submit">Login</button>
+</form>
