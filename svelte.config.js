9a56cd04dc883d72bdfb3ac489beceed60dff036
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-10-19 14:30:05 +0200

    dotenv

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-cloudflare';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
