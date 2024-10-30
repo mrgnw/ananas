e986ac474f14bf3f3300559f62101e07f9238637
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-10-30 23:21:27 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-		import NewLang from '$jibs/NewLang.svelte';
-    
-    export let data: PageData;
+	import type { PageData } from './$types';
+	import NewLang from '$jibs/NewLang.svelte';
+	export let data: PageData;
 
 
 </script>
