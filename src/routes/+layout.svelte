abdfd17d2b4993aa0e719ba19ce5ee4ba59528ce
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-11 23:46:02 +0100

    clear cache

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
-<script>import "../app.pcss";
+<script>
+	import "../app.pcss";
+	import ClearCacheButton from "$lib/components/ClearCacheButton.svelte";
 	/** @type {{children?: import('svelte').Snippet}} */
-	let { children } = $props();</script>
+	let { children } = $props();
+</script>
 <svelte:head>
-<link rel="icon" href="/favicon.ico" sizes="32x32">
-<link rel="icon" href="/icon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/icon-180.png">
-<link rel="manifest" href="/manifest.webmanifest">
+	<link rel="icon" href="/favicon.ico" sizes="32x32">
+	<link rel="icon" href="/icon.svg" type="image/svg+xml">
+	<link rel="apple-touch-icon" href="/icon-180.png">
+	<link rel="manifest" href="/manifest.webmanifest">
 </svelte:head>
 {@render children?.()}
+<ClearCacheButton />
