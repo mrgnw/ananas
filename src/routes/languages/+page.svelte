2ea59b6fb74776aa4d10c49945bc9c2edb3d4f8f
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-25 14:57:38 +0100

    more settings

--- src/routes/languages/+page.svelte
+++ src/routes/languages/+page.svelte
@@ -82,28 +82,6 @@ function clearLocalStorageCache() {
 <div class="container mx-auto p-4" in:fade>
     <div class="flex justify-between items-center mb-6">
         <h1 class="text-3xl font-bold">Languages</h1>
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button variant="outline" size="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem on:click={addDefaultLanguages}>
-                    Add Default Languages
-                </DropdownMenuItem>
-                <DropdownMenuItem on:click={resetLanguages}>
-                    Reset to Defaults
-                </DropdownMenuItem>
-                <DropdownMenuItem class="text-destructive" on:click={clearAllLanguages}>
-                    Clear All Languages
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem class="text-destructive" on:click={clearLocalStorageCache}>
-                    Clear Cache
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
     </div>
     
     <div class="mb-6">
