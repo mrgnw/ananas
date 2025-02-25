1d85559eefc6508b3eb9e974320ebfbfb54b790d
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-25 19:53:41 +0100

    add iso, iso3 and speakers_m

--- scripts/fetch-wikidata-country-data.js
+++ scripts/fetch-wikidata-country-data.js
@@ -11,13 +11,16 @@
 const WIKIDATA_ENDPOINT = 'https://query.wikidata.org/sparql';
 
 const QUERY = `
-SELECT DISTINCT ?country ?countryLabel ?isoCode ?flagImage
+SELECT DISTINCT ?country ?countryLabel ?isoCode ?iso3Code ?flagImage
 (GROUP_CONCAT(DISTINCT ?lang; separator="|") as ?languages)
 (GROUP_CONCAT(DISTINCT ?langLabel; separator="|") as ?languageLabels)
+(GROUP_CONCAT(DISTINCT ?langIso; separator="|") as ?languageIsos)
+(GROUP_CONCAT(DISTINCT ?langIso1; separator="|") as ?languageIso1s)
 (GROUP_CONCAT(DISTINCT ?speakers; separator="|") as ?speakerCounts)
 WHERE {
   ?country wdt:P31 wd:Q3624078 .  # Instance of: sovereign state
   ?country wdt:P297 ?isoCode .     # ISO 3166-1 alpha-2 code
+  ?country wdt:P298 ?iso3Code .    # ISO 3166-1 alpha-3 code
   
   # Get flag image
   OPTIONAL { ?country wdt:P41 ?flagImage . }
@@ -27,6 +30,10 @@ WHERE {
     ?country wdt:P37 ?lang .       # Official language
     ?lang rdfs:label ?langLabel .  
     FILTER(LANG(?langLabel) = "en")
+    
+    # Get ISO codes for languages
+    OPTIONAL { ?lang wdt:P220 ?langIso }   # ISO 639-3 code
+    OPTIONAL { ?lang wdt:P218 ?langIso1 }  # ISO 639-1 code
     
     # Get speaker counts where available
     OPTIONAL { 
@@ -39,7 +46,7 @@ WHERE {
     bd:serviceParam wikibase:language "en" .
   }
 }
-GROUP BY ?country ?countryLabel ?isoCode ?flagImage
+GROUP BY ?country ?countryLabel ?isoCode ?iso3Code ?flagImage
 ORDER BY ?countryLabel
 `;
 
@@ -64,22 +71,28 @@ async function fetchWikidataCountries() {
   const countries = data.results.bindings.map(country => {
     const languages = country.languages?.value.split('|').filter(Boolean) || [];
     const languageLabels = country.languageLabels?.value.split('|').filter(Boolean) || [];
+    const languageIsos = country.languageIsos?.value.split('|').filter(Boolean) || [];
+    const languageIso1s = country.languageIso1s?.value.split('|').filter(Boolean) || [];
     const speakerCounts = country.speakerCounts?.value.split('|').map(Number).filter(Boolean) || [];
     
-    // Create language objects with labels and speaker counts
+    // Create language objects with labels, ISO codes and speaker counts
     const languagesWithData = languages.map((lang, index) => ({
       id: lang.split('/').pop(),
       name: languageLabels[index],
-      speakers: speakerCounts[index] || null
+      iso: languageIsos[index],
+      iso1: languageIso1s[index] || null,
+      speakers: speakerCounts[index] || null,
+      speakers_m: speakerCounts[index] ? +(speakerCounts[index] / 1000000).toFixed(1) : null
     }));
-
+  
     // Sort languages by speaker count
     languagesWithData.sort((a, b) => (b.speakers || 0) - (a.speakers || 0));
-
+  
     return {
       wikidata_id: country.country.value.split('/').pop(),
       name: country.countryLabel.value,
-      code: country.isoCode.value,
+      iso: country.isoCode.value,
+      iso3: country.iso3Code.value,
       flag: String.fromCodePoint(
         ...country.isoCode.value
           .toUpperCase()
